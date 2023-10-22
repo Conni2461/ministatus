bd884bc7f53009c21b1d54f468fb02450ebfc3cb
Simon Hauser <Simon-Hauser@outlook.de>
2023-10-22 15:48:06 +0200

    chore: init ministatus

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> {} }: let
+  unstable = pkgs.unstable or (import <nixpkgs-unstable> { });
+  rustPackages = pkgs.rustPackages_1_66 or pkgs.rustPackages;
+in pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    # Compiler and linker
+    rustPackages.rustc
+    clang_15
+    # Native dependencies
+    pkg-config
+    rustPackages.cargo
+    # Utilities
+    cargo-deny
+    cargo-watch
+    cargo-outdated
+    rustPackages.clippy
+    rustPackages.rustfmt
+  ];
+
+  RUST_SRC_PATH = "${rustPackages.rustPlatform.rustLibSrc}";
+}
