dc8c81f5d7b7ac21af6645ca5237d66b703506c0
Simon Hauser <Simon-Hauser@outlook.de>
2023-10-25 16:45:26 +0200

    feat: refresh weather data every 4h

--- ministatus/src/block/weather.rs
+++ ministatus/src/block/weather.rs
@@ -1,45 +1,80 @@
+use std::sync::atomic::{AtomicI32, Ordering};
+use std::sync::Arc;
+
+use tokio::sync::RwLock;
+
+// every 4h (timeout 10s)
+const TIMEOUT_TIME: i32 = 1440;
+
 pub struct Weather {
-    data: Vec<String>,
+    data: Arc<RwLock<Vec<String>>>,
     rain_regex: regex::Regex,
     temp_regex: regex::Regex,
+
+    timeout: AtomicI32,
+}
+
+async fn get_weather_data() -> Result<Vec<String>, anyhow::Error> {
+    let o = reqwest::Client::new()
+        .get("https://wttr.in")
+        .header("Accept", "text/plain")
+        .header("User-Agent", "curl/8.1.1")
+        .send()
+        .await?
+        .text()
+        .await?;
+    Ok(o.lines().map(ToOwned::to_owned).collect::<Vec<String>>())
 }
 
 impl Weather {
     pub async fn new() -> Result<Box<Self>, anyhow::Error> {
         Ok(Box::new(Self {
-            data: Self::get_weather_data().await.unwrap_or_default(),
+            data: Arc::new(RwLock::new(get_weather_data().await.unwrap_or_default())),
             rain_regex: regex::Regex::new(r"(\d+%)")?,
             temp_regex: regex::Regex::new(r"(\+\d+)")?,
+
+            timeout: AtomicI32::new(TIMEOUT_TIME),
         }))
     }
 
-    async fn get_weather_data() -> Result<Vec<String>, anyhow::Error> {
-        let o = reqwest::Client::new()
-            .get("https://wttr.in")
-            .header("Accept", "text/plain")
-            .header("User-Agent", "curl/8.1.1")
-            .send()
-            .await?
-            .text()
-            .await?;
-        Ok(o.lines().map(ToOwned::to_owned).collect::<Vec<String>>())
+    fn refresh_data(&self) {
+        self.timeout.fetch_sub(1, Ordering::SeqCst);
+        if self.timeout.load(Ordering::Relaxed) == 0 {
+            self.timeout.store(TIMEOUT_TIME, Ordering::Relaxed);
+
+            let d = self.data.clone();
+            tokio::spawn(async move {
+                let new = get_weather_data().await.unwrap_or_default();
+                if new.is_empty() {
+                    return;
+                }
+
+                let mut w = d.write().await;
+                *w = new;
+            });
+        }
     }
 }
 
 #[async_trait::async_trait]
 impl super::Block for Weather {
     async fn run(&self) -> Result<Option<String>, anyhow::Error> {
-        // TODO: refresh data every hour or so
-        let Some(r) = self.data.get(15) else {
+        let (r, h) = if let (Some(r), Some(h)) = {
+            let d = self.data.read().await;
+            (
+                d.get(15).map(ToOwned::to_owned),
+                d.get(12).map(ToOwned::to_owned),
+            )
+        } {
+            (r, h)
+        } else {
             return Ok(None);
         };
-        let Some(h) = self.data.get(12) else {
-            return Ok(None);
-        };
+        self.refresh_data();
 
         let rain = self
             .rain_regex
-            .find_iter(r)
+            .find_iter(&r)
             .filter_map(|m| m.as_str()[..m.len() - 1].parse::<i32>().ok())
             .max();
         let Some(rain) = rain else {
@@ -48,7 +83,7 @@ impl super::Block for Weather {
 
         let temp = self
             .temp_regex
-            .find_iter(h)
+            .find_iter(&h)
             .filter_map(|m| m.as_str()[1..].parse::<i32>().ok())
             .collect::<Vec<_>>();
         let Some(min) = temp.iter().min() else {
