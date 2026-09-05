aa5a7d25e66ee56457644f9c498822f557ebe33f
Simon Hauser <simon.hauser@helsinki-systems.de>
2026-09-05 12:55:23 +0200

    chore: replace chrono with jiff

--- src/block/clock.rs
+++ src/block/clock.rs
@@ -6,21 +6,18 @@ impl Clock {
     }
 }
 
-fn render<Tz: chrono::TimeZone>(now: &chrono::DateTime<Tz>, opts: super::Options) -> String
-where
-    Tz::Offset: std::fmt::Display,
-{
+fn render(now: &jiff::civil::DateTime, opts: super::Options) -> String {
     let fmt = if opts.compact {
         "%m/%d %H:%M"
     } else {
         "(KW%V) %m/%d/%Y %I:%M %p"
     };
-    format!("🕛 {}", now.format(fmt))
+    format!("🕛 {}", now.strftime(fmt))
 }
 
 impl super::Block for Clock {
     fn run(&self, opts: super::Options) -> Result<Option<String>, anyhow::Error> {
-        Ok(Some(render(&chrono::offset::Local::now(), opts)))
+        Ok(Some(render(&jiff::Zoned::now().datetime(), opts)))
     }
 }
 
@@ -29,13 +26,13 @@ mod tests {
     use super::render;
     use crate::block::Options;
 
-    fn at(s: &str) -> chrono::DateTime<chrono::FixedOffset> {
-        chrono::DateTime::parse_from_rfc3339(s).unwrap()
+    fn at(s: &str) -> jiff::civil::DateTime {
+        s.parse().unwrap()
     }
 
     #[test]
     fn full_keeps_week_number_year_and_meridiem() {
-        let now = at("2026-09-05T12:32:00+02:00");
+        let now = at("2026-09-05T12:32:00");
         assert_eq!(
             render(&now, Options::default()),
             "🕛 (KW36) 09/05/2026 12:32 PM"
@@ -44,7 +41,7 @@ mod tests {
 
     #[test]
     fn compact_drops_week_number_and_year_and_uses_24h() {
-        let now = at("2026-09-05T13:32:00+02:00");
+        let now = at("2026-09-05T13:32:00");
         assert_eq!(render(&now, Options { compact: true }), "🕛 09/05 13:32");
     }
 }
